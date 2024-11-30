38dc1db48e65934dc401f41d90dd19acce35659b
Daniel Santamarta <danisantamarta5@gmail.com>
2024-11-30 12:16:04 +0100

    acabamos la base del programa colocando la piezas

--- js/app.js
+++ js/app.js
@@ -14,15 +14,22 @@ const colores = {
     oscuro: '#769656'
 }
 
+const coloresPiezas = {
+    claro: '#ffffff',
+    oscuro: '#000000'
+}
+
 //Piezas escritas en Unicode
 const piezas = {
-    rey: '&#9812',
-    reina: '&#9813',
-    torre: '&#9814',
-    alfil: '&#9815',
-    caballo: '&#9816',
-    peon: '&#9817',
+    rey: ['♚','♔'],
+    reina: ['♛','♕'],
+    torre: ['♜','♖'],
+    alfil: ['♝','♗'],
+    caballo: ['♞','♘'],
+    peon: ['♟','♙'],
 }
+
+const posicionarPiezas = altoCelda * .1;
 
 const $canvas = document.createElement('canvas');
 const ctx = $canvas.getContext('2d');
@@ -36,11 +43,18 @@ ctx.textBaseline = 'middle';
 ctx.textAlign = 'center';
 
 document.body.appendChild($canvas);
+
+document.body.style.display = 'flex';
+document.body.style.justifyContent = 'center';
+document.body.style.alignContent = 'center';
+document.body.style.height = '100%';
+document.body.parentElement.style.height = '100%';
+document.body.style.background = '#333333'
+
   
 const renderizarTablero = () =>{
     for (let x=0; x<filas; x++){
         for (let y=0; y<columnas; y++){
-            tableroMatriz[x][y] = null;
             let colorCelda = colores.claro;
             let colorTexto = colores.oscuro;
     
@@ -52,20 +66,24 @@ const renderizarTablero = () =>{
             ctx.fillStyle = colorCelda;
             ctx.fillRect(x * anchoCelda ,y * altoCelda ,anchoCelda ,altoCelda);
             
+            //dibujar posicion base
             ctx.fillStyle = colorTexto;
-
             ctx.textBaseline = 'top';
             ctx.textAlign = 'start';
             ctx.font = '8px Arial';
             ctx.fillText(`[${x};${y}]`,x * anchoCelda + 10,y * altoCelda + 10);
 
-           
+           //dibujar pieza
             const pieza = tableroMatriz[x][y];
             if(pieza){
+                ctx.fillStyle = pieza.color;
                 ctx.textBaseline = 'middle';
                 ctx.textAlign = 'center';
-                ctx.font = '64px Arial';
-                ctx.fillText(pieza.tipo,x * anchoCelda + altoCelda / 2 ,y * altoCelda + anchoCelda / 2 );    
+                ctx.font = '72px Arial';
+                ctx.fillStyle = pieza.color;
+                ctx.fillText(pieza.tipo[0],x * anchoCelda + altoCelda / 2 ,y * altoCelda + anchoCelda / 2 + posicionarPiezas );    
+                ctx.fillStyle = coloresPiezas.oscuro;
+                ctx.fillText(pieza.tipo[1],x * anchoCelda + altoCelda / 2 ,y * altoCelda + anchoCelda / 2 + posicionarPiezas );    
             }
 
         }
@@ -83,9 +101,81 @@ for (let x=0; x<filas; x++){
 }
 
 //Colocar las piezas
+for(let i = 0; i < columnas; i++){
+    tableroMatriz[i][1] = {
+        tipo: piezas.peon,
+        color: coloresPiezas.oscuro,
+    }
+    tableroMatriz[i][6] = {
+        tipo: piezas.peon,
+        color: coloresPiezas.claro,
+    }
+}
+
 tableroMatriz[0][0] = {
-    tipo: piezas.peon,
-    color: colores.claro,
+    tipo: piezas.torre,
+    color: coloresPiezas.oscuro,
+}
+tableroMatriz[1][0] = {
+    tipo: piezas.caballo,
+    color: coloresPiezas.oscuro,
+}
+tableroMatriz[2][0] = {
+    tipo: piezas.alfil,
+    color: coloresPiezas.oscuro,
+}
+tableroMatriz[3][0] = {
+    tipo: piezas.reina,
+    color: coloresPiezas.oscuro,
+}
+tableroMatriz[4][0] = {
+    tipo: piezas.rey,
+    color: coloresPiezas.oscuro,
+}
+tableroMatriz[5][0] = {
+    tipo: piezas.alfil,
+    color: coloresPiezas.oscuro,
+}
+tableroMatriz[6][0] = {
+    tipo: piezas.caballo,
+    color: coloresPiezas.oscuro,
+}
+tableroMatriz[7][0] = {
+    tipo: piezas.torre,
+    color: coloresPiezas.oscuro,
+}
+
+tableroMatriz[0][7] = {
+    tipo: piezas.torre,
+    color: coloresPiezas.claro,
+}
+tableroMatriz[1][7] = {
+    tipo: piezas.caballo,
+    color: coloresPiezas.claro,
+}
+tableroMatriz[2][7] = {
+    tipo: piezas.alfil,
+    color: coloresPiezas.claro,
+}
+tableroMatriz[3][7] = {
+    tipo: piezas.reina,
+    color: coloresPiezas.claro,
+}
+tableroMatriz[4][7] = {
+    tipo: piezas.rey,
+    color: coloresPiezas.claro,
+}
+tableroMatriz[5][7] = {
+    tipo: piezas.alfil,
+    color: coloresPiezas.claro,
+}
+tableroMatriz[6][7] = {
+    tipo: piezas.caballo,
+    color: coloresPiezas.claro,
+}
+tableroMatriz[7][7] = {
+    tipo: piezas.torre,
+    color: coloresPiezas.claro,
 }
 
 renderizarTablero();
